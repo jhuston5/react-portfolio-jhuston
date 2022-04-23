b193a67e05f4cd298e614839981da2f843820f4d
Joshua Huston <joshua.david.huston@gmail.com>
2022-04-23 18:12:57 -0400

    Adjusted component casing

--- components/layout.js
+++ components/layout.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
+import About from './About'
 
 const name = '[Your Name]'
 export const siteTitle = 'Next.js Sample Website'
@@ -49,6 +50,12 @@ export default function Layout({ children, home }) {
                   height={108}
                   width={108}
                   alt={name}
+                />
+              </a>
+            </Link>
+            <Link href="/">
+              <a>
+                <About
                 />
               </a>
             </Link>
